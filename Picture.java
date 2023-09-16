b13efbbd9a17428054fc97152e273e74654628fb
Daniel C <63077752+dcrep@users.noreply.github.com>
2023-09-15 23:34:07 -0400

    Added grass, modified animation
    Added grass and modified animations to be slightly less annoying

--- Picture.java
+++ Picture.java
@@ -9,6 +9,7 @@
  */
 public class Picture
 {
+    private Square grassArray[];
     private Square wall;
     private Square door;
     private Square window;
@@ -22,6 +23,10 @@ public class Picture
      */
     public Picture()
     {
+        grassArray = new Square[4];
+        for (int i = 0; i < grassArray.length; i++) {
+            grassArray[i] = new Square();
+        }
         wall = new Square();
         door = new Square();
         window = new Square();
@@ -37,38 +42,58 @@ public class Picture
     public void draw()
     {
         if(!drawn) {
+            int horizDist = -160;
+            for (Square s: grassArray) {
+                s.changeColor("green");
+                s.changeSize(40);
+                // move vertically BEFORE showing
+                s.moveVertical(140);
+                s.makeVisible();
+                // This makes it uber annoying:
+                //s.slowMoveVertical(140);
+                s.slowMoveHorizontal(horizDist);
+                horizDist += 40;
+            }
             wall.changeSize(120);
+            wall.moveHorizontal(-140);
+            wall.moveVertical(-100);    // fall from above
             wall.makeVisible();
-            wall.slowMoveHorizontal(-140);
-            wall.slowMoveVertical(20);
+            //wall.slowMoveHorizontal(-140);
+            wall.slowMoveVertical(100 + 20);
             
             door.changeColor("blue");
             door.changeSize(35);
+            door.moveVertical(105);
             door.makeVisible();
             door.slowMoveHorizontal(-70);
-            door.slowMoveVertical(105);            
+            //door.slowMoveVertical(105);            
             
             window.changeColor("black");
             window.changeSize(30);
+            window.moveVertical(40);
             window.makeVisible();
             window.slowMoveHorizontal(-120);
-            window.slowMoveVertical(40);            
+            //window.slowMoveVertical(40);            
     
             roof.changeSize(60, 180);
+            roof.moveHorizontal(20);
             roof.makeVisible();
-            roof.slowMoveHorizontal(20);
+            //roof.slowMoveHorizontal(20);
             roof.slowMoveVertical(-60);
             
             sun.changeColor("yellow");
             sun.changeSize(80);
+            sun.moveHorizontal(100);
+            sun.moveVertical(120);  // make it "rise"
             sun.makeVisible();
-            sun.slowMoveHorizontal(100);
-            sun.slowMoveVertical(-40);            
+            //sun.slowMoveHorizontal(100);
+            sun.slowMoveVertical(-120 + -40);            
             
             personA.changeColor("orange");
             personA.changeSize(60,30);
+            personA.moveHorizontal(-50);
             personA.makeVisible();
-            personA.slowMoveHorizontal(-50);
+            //personA.slowMoveHorizontal(-50);
             personA.slowMoveVertical(60);
             
             drawn = true;
@@ -80,6 +105,9 @@ public class Picture
      */
     public void setBlackAndWhite()
     {
+        for (Square s: grassArray) {
+            s.changeColor("black");
+        }
         wall.changeColor("black");
         door.changeColor("white");
         window.changeColor("white");
@@ -93,6 +121,9 @@ public class Picture
      */
     public void setColor()
     {
+        for (Square s: grassArray) {
+            s.changeColor("green");
+        }
         wall.changeColor("red");
         door.changeColor("blue");
         window.changeColor("black");
